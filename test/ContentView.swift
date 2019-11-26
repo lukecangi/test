79375dcee6c92e7ab199af5d0da44e74420f80a6
Luke Cangi <lukecangi@icloud.com>
2019-11-26 17:55:45 -0600

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Luke Cangi on 11/26/19.
+//  Copyright © 2019 Luke Cangi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
